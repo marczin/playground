3ea2fa75f5d227d0dc7a074f31e86649872cd40e
marczin <marcinrosol1997@gmail.com>
2020-06-03 12:54:26 +0200

    eureka

--- eureka/movie-catalog-service/src/main/java/me/marcinrosol/moviecatalogservice/resources/MovieCatalogResource.java
+++ eureka/movie-catalog-service/src/main/java/me/marcinrosol/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,5 +1,6 @@
 package me.marcinrosol.moviecatalogservice.resources;
 
+import com.netflix.discovery.DiscoveryClient;
 import me.marcinrosol.moviecatalogservice.models.CatalogItem;
 import me.marcinrosol.moviecatalogservice.models.Movie;
 import me.marcinrosol.moviecatalogservice.models.Rating;
@@ -25,17 +26,20 @@ public class MovieCatalogResource {
     private RestTemplate restTemplate;
 
     @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
     private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
         //get all rated move ids
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://movie-ratings-service/ratingsdata/users/"+userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
             // for each movie id, call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 
             //put them all together
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
